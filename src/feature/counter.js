1cedcd7830d6588ae694b08090407d9c888d002f
spacestronaut <abelr6099@gmail.com>
2021-10-01 09:33:32 +0700

    add localStorage

--- src/feature/counter.js
+++ src/feature/counter.js
@@ -3,11 +3,11 @@ import { createSlice } from "@reduxjs/toolkit";
 export const counterSlice = createSlice({
     name : 'counter',
     initialState : {
-        value : 0
+        value : parseInt(localStorage.getItem('value')) ? parseInt(localStorage.getItem('value')) : 0 
     },
     reducers : {
         increment : (state) => {
-            state.value +=1
+            localStorage.setItem('value', state.value +=1)
         }
     }
 })
