aec38ac5b06a1a03cca20b03e704a6a5134985b9
spacestronaut <abelr6099@gmail.com>
2021-10-01 09:27:21 +0700

    add redux

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import Router from './routes/index'
+import { Provider } from 'react-redux'
+import store from './store/store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router />
+    <Provider store={store}>
+      <Router />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,16 @@
 import { Link } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { increment } from '../feature/counter'
 
 export default function Home() {
+    const count = useSelector((state) => state.counter.value)
+    const dispatch = useDispatch()
+
     return (
         <div className="flex">
             <Link to="/about" className="bg-blue-400 text-white py-2 px-4 rounded hover:bg-blue-600 focus:bg-blue-700 focus:ring-blue-700">About</Link>
-            Home
+            <span>{count}</span>
+            <button className="bg-red-400 text-white py-2 px-4 rounded hover:bg-red-600 focus:bg-red-800" onClick={() => dispatch(increment())}>+</button>
         </div>
     )
 }
